ff2dde5593c1707047d251632dae8c802369402b
Salmon13 <dodo.0713.2000@gmail.com>
2023-06-09 13:22:11 +0800

    add: generate prompt for GPT-4

--- src/package_insert.py
+++ src/package_insert.py
@@ -31,5 +31,52 @@ def get_dose_NL(drug_name: str, output_path=None) -> str:
         print(f"[Info] Cannot get message of {output_path} from openFDA.", file=sys.stderr)
         return ""
     
+def generate_prompt(drug_name: str, dosage_info: str) -> str:
+    prompt_content = f"""Between >>> and <<< is a drug dosage information of {drug_name} for different situations. I need you to extract the column info for different situtation.
+    Use array of json object to answer my question. The columns and their definitions are as follow:
+    | drug | age | gender | renal_impairment | hepatic_impairment | special_disease | weight | dosage | max_dose | recommend |
+    - drug:
+        - type: TEXT
+        - content: drug name
+    - age:
+        - type: TEXT
+        - content:
+            if the age range is provided, use the format "<min>~<max>" to represent. <min> and <max> are arabic number representing the minimum and the maximum age seperately, for example: "13~17"
+            if the age range is not provided, fill in "pediatric" or "adult" or "elderly" depending on your self judge
+            if there is no any information about age, fill in "any" to represent
+    - gender:
+        - type: TEXT
+        - content: fill in "male" or "female" or "any" if the gender is not specifically mentioned
+    - renal_impairment
+        - type: TEXT array
+        - content: the possible element are mild/moderate/severe/ESRD, the array can contain multiple severity, leave it an empty array [] if it has nothing to do with renal impairment
+    - hepatic_impairment
+        - type: TEXT array
+        - content: the possible element are mild/moderate/severe, the array can contain multiple severity, leave it an empty array [] if it has nothing to do with hepatic impairment
+    - special_disease
+        - type: TEXT array
+        - content: the possible element are ADHD/MDD/GAD/PD/PTSD/debilitated, put the special disease into the array if the dosage information is for the patient with that special disease
+    - weight:
+        - type: TEXT
+        - content:
+            if the weight range is provided, use the format "<min>~<max>" to represent. <min> and <max> are arabic number representing the minimum and the maximum weight seperately, for example: "48~71"
+            if there is no any information about weight, fill in "any" to represent
+    - weight_unit:
+        - type: TEXT
+        - content: the unit of the weight, for example "lb". fill in "N/A" if the information is not provided
+    - dosage:
+        - type: TEXT
+        - content: the dosage information for this situation, should always have content
+    - max_dosage:
+        - type: TEXT
+        - content: the maximum dosage information for this situation, leave it "N/A" if no proper information
+    - recommend:
+        - type: TEXT
+        - content: fill in other recommendation that can be shown in the above informations
+    >>>{dosage_info}<<<
+    """
+    return prompt_content
+
 if __name__ == "__main__":
-    print(type(get_dose_NL("Adderall")))
+    drug_name = "Adderall"
+    print(generate_prompt(drug_name, get_dose_NL(drug_name)))
